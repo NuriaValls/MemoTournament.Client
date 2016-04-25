8dc187d1725ab4f1c136512d74acb0c37b7c094b
NuriaValls <nvallscanudas@hotmail.com>
2016-04-25 17:15:46 +0200

    vista client

--- src/view/MainViewClient.java
+++ src/view/MainViewClient.java
@@ -1,7 +1,10 @@
 package view;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import controller.MainViewControllerC;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
@@ -14,12 +17,22 @@ public class MainViewClient extends JFrame{
 	private static JPanel jpMenu;
 	private static CardLayout cardLayout = new CardLayout();
 	
-	private static JPanel jpRegisterCard = new JPanel();
 	private static JPanel jpLogInCard = new JPanel();
+	private static JPanel jpRegisterCard = new JPanel();
+	private static JPanel jpInitialMenuCard = new JPanel();
+	private static JPanel jpSelectGameCard = new JPanel();
 	private static JPanel jpGameCard = new JPanel();
 	private static JPanel jpRankingCard = new JPanel();
 	
 	private JPanel jpButtonMenu;
+	
+	private static JButton jbRegister = new JButton("Register");
+	private static JButton jbNewGame = new JButton("New Game");
+	private static JButton jbRanking = new JButton("Show Ranking");
+	private static JButton jbLogOut = new JButton("Log Out");
+	private static JButton jbBack = new JButton("Back");
+	private static JButton jbGoToMenu = new JButton("Go to Menu");
+	private static JButton jbStartGame = new JButton("Start Game");
 	
 	public MainViewClient(){
 		setTitle("Memory Torunament -Client-");
@@ -27,15 +40,53 @@ public class MainViewClient extends JFrame{
 		setLocationRelativeTo(null);
 		setSize(400,300);
 		
-		createRegisterCard();
 		createLogInCard();
+		createRegisterCard();
+		createInitialMenuCard();
+		createSelectGameCard();
 		createGameCard();
 		createRankingCard();
 		
 		jpMenu = new JPanel();
 		jpMenu.setLayout(cardLayout);
 		
-		//add aux panels
+		jpMenu.add(jpLogInCard, "1");
+		jpMenu.add(jpRegisterCard, "2");
+		jpMenu.add(jpInitialMenuCard, "3");
+		jpMenu.add(jpSelectGameCard, "4");
+		jpMenu.add(jpGameCard, "5");
+		jpMenu.add(jpRankingCard, "6");
+		
+		jpButtonMenu = new JPanel();
+		
+		jpButtonMenu.add(jbGoToMenu);
+		jpButtonMenu.add(jbRegister);
+		jpButtonMenu.add(jbBack);
+		jpButtonMenu.add(jbNewGame);
+		jpButtonMenu.add(jbStartGame);
+		jpButtonMenu.add(jbRanking);
+		jpButtonMenu.add(jbLogOut);
+		
+		jbBack.setVisible(false);
+		jbNewGame.setVisible(false);
+		jbStartGame.setVisible(false);
+		jbRanking.setVisible(false);
+		jbLogOut.setVisible(false);
+		
+		jpButtonMenu.setVisible(true);
+		
+		add(jpButtonMenu, BorderLayout.SOUTH);
+		add(jpMenu, BorderLayout.NORTH);
+		pack();
+	}
+	
+	public void registerController(MainViewControllerC actionListener){
+		jbRegister.addActionListener(actionListener);
+		//jbLogIn.addActionListener(actionListener);
+		//jbGuest.addActionListener(actionListener);
+		jbNewGame.addActionListener(actionListener);
+		jbRanking.addActionListener(actionListener);
+		jbLogOut.addActionListener(actionListener);
 	}
 	
 	public void createRegisterCard(){
@@ -43,6 +94,14 @@ public class MainViewClient extends JFrame{
 	}
 	
 	public void createLogInCard(){
+		
+	}
+	
+	public void createInitialMenuCard(){
+		
+	}
+	
+	public void createSelectGameCard(){
 		
 	}
 	
