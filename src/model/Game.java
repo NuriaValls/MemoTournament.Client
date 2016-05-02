f5bf8c0dbe6fb3496ff16d7d6424b1b2f03edf9c
luisrecolons <luisreco96@gmail.com>
2016-05-02 13:13:20 +0200

    game

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,40 +1,39 @@
 package model;
 
-import javax.swing.JButton;
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
-public class Card extends JButton{
-   
-	private static final long serialVersionUID = 1L;
-	private final int id;
-	private int ai = 0; //# times shown, for AI purpose
-    private boolean matched = false;
 
-    public Card(int id){
-    	this.id = id;
-    }
-
-    public int getId(){
-        return this.id;
-    }
-
-    public void setMatched(boolean matched){
-        this.matched = matched;
-    }
-
-    public boolean getMatched(){
-        return this.matched; 
-    }
-    
-    public void showText(){
-    	this.setText(String.valueOf(id));
-    }
-    
-    public void hideText(){
-    	this.setText("");
-    }
-    
-    
-    public void incrementai(){
-    	this.ai = ai++;
-    }
+public class Game{
+	private static int dimx;
+	private static int dimy;
+	private static int locx;
+	private static int locy;
+	
+    public static void main(String[] args){
+        int difficulty = 3;
+    	switch (difficulty){
+        	case 1: dimx = 600; //easy dimensions
+        			dimy = 600;
+        			locx = 450;
+        			locy = 150;
+        			break;
+        	case 2: dimx = 750; //normal dimensions
+        			dimy = 750;
+        			locx = 350;
+        			locy = 75;
+        			break;
+        	case 3: dimx = 1000; //hard dimensions
+        			dimy = 800;
+        			locx = 220;
+        			locy = 25;
+        }
+    	
+    	Board b = new Board(difficulty);
+        b.setPreferredSize(new Dimension(dimx, dimy)); //creates JFrame
+        b.setLocation(locx, locy); //starting position, must be centered in the screen
+        b.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        b.pack();
+        b.setVisible(true);
+    }   
 }
